85dc5445049e763c733f28c4c579c85d6278f22f
Leonardo0294 <chichelezcano94@hotmail.com.ar>
2024-05-12 16:44:10 -0300

    first commit

---
+++ Screens/ProductListeningScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProductListingScreen = () => {
+  return (
+    <View>
+      <Text>ProductListingScreen</Text>
+    </View>
+  );
+};
+
+export default ProductListingScreen;
+
+const styles = StyleSheet.create({});
